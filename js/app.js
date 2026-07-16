4574151b4d2439127735a57dfa2dd0d1e1b6da2d
his-imag8 <ekanemdaniel60@gmail.com>
2026-07-16 22:44:30 +0100

    Event gallery s;ider added successfully using the gsap library

--- js/app.js
+++ js/app.js
@@ -2,15 +2,20 @@
 import gsap from "../node_modules/gsap/index.js";
 import { ScrollTrigger } from "../node_modules/gsap/ScrollTrigger.js";
 import { SplitText } from "../node_modules/gsap/SplitText.js";
+import { _horizontal } from "../node_modules/gsap/Observer.js";
 
-gsap.registerPlugin(ScrollTrigger, SplitText);
+const wrapper = document.querySelector(".event-container");
+const aboutContent = document.querySelector(".about");
+
+gsap.registerPlugin(ScrollTrigger, SplitText, _horizontal);
 
 ScrollTrigger.create({
   trigger: "#menu",
   start: "top top",
   end: "bottom top",
   scrub: 8,
-  // pinSpacing: true,
+  pinSpacing: true,
+  invalidateOnRefresh: true,
   pin: true,
   // markers: {
   //   startColor: "black",
@@ -19,7 +24,7 @@ ScrollTrigger.create({
   //   fontWeight: "bold",
   //   indent: 20,
   // },
-  onUpdate: (self) => console.log("direction:", self.direction),
+  // onUpdate: (self) => console.log("direction:", self.direction),
 });
 
 gsap.to(".special-menu-item-img", {
@@ -33,3 +38,46 @@ gsap.to(".special-menu-item-img", {
     toggleActions: "restart none complete reverse",
   },
 });
+
+gsap.to(".event-container", {
+  x: () => -(wrapper.scrollWidth - window.innerWidth),
+  ease: "none",
+  markers: {
+    startColor: "black",
+    endColor: "black",
+    fontSize: "18px",
+    fontWeight: "bold",
+    indent: 20,
+  },
+
+  scrollTrigger: {
+    trigger: ".event",
+    start: "top top",
+    end: () => `+=${wrapper.scrollWidth}`,
+    pin: true,
+    scrub: 1,
+    invalidateOnRefresh: true,
+  },
+});
+
+gsap.to(".event-imgs", {});
+
+gsap.from(".about", {
+  y: "30%",
+  duration: "1.5",
+  scrub: 2,
+  start: "top, 50%",
+  end: "top bottom",
+
+  scrollTrigger: {
+    trigger: ".about-left .about-right",
+  },
+});
+
+function aboutSectionFn(entry, check) {}
+
+const observer = new IntersectionObserver(aboutSectionFn, {
+  threshold: 0.3,
+});
+
+aboutContent.observer();
