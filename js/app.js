a4979fc2bb5a82ea30f41b92eb3d528ce74f5d71
his-imag8 <ekanemdaniel60@gmail.com>
2026-07-18 02:50:09 +0100

    Hero section has been added with split text and svg icons with a smooth transition on reload

--- js/app.js
+++ js/app.js
@@ -3,20 +3,83 @@ import gsap from "../node_modules/gsap/index.js";
 import { ScrollTrigger } from "../node_modules/gsap/ScrollTrigger.js";
 import { SplitText } from "../node_modules/gsap/SplitText.js";
 import { _horizontal } from "../node_modules/gsap/Observer.js";
+gsap.registerPlugin(ScrollTrigger, SplitText, _horizontal);
+
+("mycolor.space, fontpair.co");
 
 const wrapper = document.querySelector(".event-container");
 const aboutContent = document.querySelector(".about");
 
-gsap.registerPlugin(ScrollTrigger, SplitText, _horizontal);
+////////////////////
+// TO SPLIT HERO TEXT
+SplitText.create(".main-left-heading--secondary", {
+  type: "chars",
+  autoSplit: true,
+  onSplit: (self) => {
+    console.log(self);
+    return gsap.from(self.chars, {
+      y: 100,
+      opacity: 0,
+      stagger: 0.05,
+    });
+  },
+});
+
+/////////////////
+//TO BRING BTN FROM THE BOTTOM
+gsap.from(".main-left-btn", {
+  y: 100,
+  opacity: 0,
+  delay: 1.2,
+  stagger: 1,
+});
+
+/////////////////
+// TO FLEX IMAGE FROM THE RIGHT
+gsap.from(".main-right", {
+  x: 100,
+  opacity: 0,
+  delay: 1.5,
+  stagger: 1,
+});
+
+/////////////
+// ICONS OPACITY
+gsap.from(".icon", {
+  opacity: 0,
+  delay: 1.5,
+  stagger: 0.2,
+  transition: 0.2,
+});
+
+/////////////
+// TO SPLIT BY WORDS IN SUB HERO TEXT
+SplitText.create(".main-left-heading--tertiary", {
+  type: "lines",
+  autoSplit: true,
+  onSplit: (self) => {
+    console.log(self);
+    return gsap.from(self.lines, {
+      y: 100,
+      opacity: 0,
+      stagger: 0.05,
+      delay: 1.5,
+    });
+  },
+});
+
+requestAnimationFrame(() => {
+  ScrollTrigger.refresh();
+});
 
 ScrollTrigger.create({
   trigger: "#menu",
   start: "top top",
   end: "bottom top",
-  scrub: 8,
-  pinSpacing: true,
-  invalidateOnRefresh: true,
+  scrub: 1,
+  // invalidateOnRefresh: true,
   pin: true,
+  pinSpacing: true,
   // markers: {
   //   startColor: "black",
   //   endColor: "black",
@@ -60,24 +123,22 @@ gsap.to(".event-container", {
   },
 });
 
-gsap.to(".event-imgs", {});
+// gsap.to(".event-imgs", {});
 
 gsap.from(".about", {
   y: "30%",
-  duration: "1.5",
-  scrub: 2,
-  start: "top, 50%",
-  end: "top bottom",
+  duration: "1",
 
   scrollTrigger: {
-    trigger: ".about-left .about-right",
+    trigger: ".about-container",
+    start: "top, 70%",
+    end: "bottom bottom",
+    // markers: {
+    //   startColor: "green",
+    //   endColor: "black",
+    //   fontSize: "18px",
+    //   fontWeight: "bold",
+    //   indent: 20,
+    // },
   },
 });
-
-function aboutSectionFn(entry, check) {}
-
-const observer = new IntersectionObserver(aboutSectionFn, {
-  threshold: 0.3,
-});
-
-aboutContent.observer();
