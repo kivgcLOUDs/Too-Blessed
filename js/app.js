91c158d4859c21d8163154271fab2ed4da394772
his-imag8 <ekanemdaniel60@gmail.com>
2026-07-23 17:46:09 +0100

    done with gallery layout and is about switching to get previous code

--- js/app.js
+++ js/app.js
@@ -1,15 +1,19 @@
-import { gsap } from "../node_modules/gsap";
+// import { gsap } from "../node_modules/gsap";
 
-// import { ScrollTrigger } from "../node_modules/gsap/gsap-core/ScrollTrigger";
-// // ScrollSmoother requires ScrollTrigger
-// import { ScrollSmoother } from "../node_modules/gsap/gsap-core/ScrollSmoother";
-// import { SplitText } from "../node_modules/gsap/gsap-core/SplitText";
-// import { TextPlugin } from "../node_modules/gsap/gsap-core/TextPlugin";
+import gsap from "../node_modules/gsap/index.js";
+import { ScrollTrigger } from "../node_modules/gsap/ScrollTrigger.js";
+import { SplitText } from "../node_modules/gsap/SplitText.js";
+import { _horizontal } from "../node_modules/gsap/Observer.js";
+gsap.registerPlugin(ScrollTrigger, SplitText, _horizontal);
 
 gsap.to(".gallery-content", {
-  backgroundColor: "blue",
+  pin: true,
+  backgroundColor: "#421301",
+  stagger: 1,
 
   scrollTrigger: {
+    start: "top top",
+    end: "bottom top",
     trigger: ".gallery",
     markers: true,
   },
