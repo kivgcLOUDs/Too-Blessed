84c4e6e3ca6070786553c7faa6855086fbdb2f84
his-imag8 <ekanemdaniel60@gmail.com>
2026-07-24 18:00:14 +0100

    Added the palms image in the hero section and made the hero layout more responsive

--- js/app.js
+++ js/app.js
@@ -6,15 +6,50 @@ import { SplitText } from "../node_modules/gsap/SplitText.js";
 import { _horizontal } from "../node_modules/gsap/Observer.js";
 gsap.registerPlugin(ScrollTrigger, SplitText, _horizontal);
 
-gsap.to(".gallery-content", {
-  pin: true,
-  backgroundColor: "#421301",
-  stagger: 1,
+const wrapper = document.querySelector(".gallery");
+const galleryContainer = document.querySelector(".gallery-section");
+
+gsap.to(".gallery", {
+  x: () => -(wrapper.scrollWidth - window.innerWidth),
+  ease: "none",
+  // stagger: 1,
 
   scrollTrigger: {
+    trigger: ".gallery-section",
     start: "top top",
+    end: () => `+=${wrapper.scrollWidth}`,
+
+    // onEnterBack: () => (galleryContainer.style.backgroundColor = "#ff6b35"),
+    // onLeaveBack: () => (galleryContainer.style.backgroundColor = "#eee"),
+    // onEnter: () => (galleryContainer.style.backgroundColor = "#000"),
+
+    toggleClass: {
+      targets: ".gallery-section",
+      className: "active",
+    },
+
+    pin: true,
+    scrub: 1,
+    markers: false,
+    invalidateOnRefresh: true,
+  },
+});
+
+gsap.from(".flexy", {
+  toggleClass: {
+    targets: ".gallery-section",
+    // opacity: 1,
+    // className: "gallery-section--active",
+  },
+
+  // y: "30%",
+  scrollTrigger: {
+    trigger: ".gallery-section",
+    onEnter: () => galleryContainer.classList.add("gallery-section--active"),
+    start: "top 50%",
     end: "bottom top",
-    trigger: ".gallery",
-    markers: true,
+    // scrub: 1,
+    // once: true,
+    // markers: true,
   },
 });
